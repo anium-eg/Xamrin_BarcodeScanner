422e490e563e4ae9229d65a74323b84cfbfcac07
Anish Umesh <anium@eg.dk>
2025-03-07 15:02:43 +0530

    minor changes

--- BU_BarcodeScanner/BU_BarcodeScanner.Android/MainActivity.cs
+++ BU_BarcodeScanner/BU_BarcodeScanner.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace BU_BarcodeScanner.Droid
 {
-    [Activity(Label = "BU_BarcodeScanner", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "BU_BarcodeScanner", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize, ScreenOrientation = ScreenOrientation.Portrait )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
--- BU_BarcodeScanner/BU_BarcodeScanner/obj/Debug/netstandard2.0/BU_BarcodeScanner.AssemblyInfo.cs
+++ BU_BarcodeScanner/BU_BarcodeScanner/obj/Debug/netstandard2.0/BU_BarcodeScanner.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BU_BarcodeScanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+feefcc9e61d89b280d2d359207c5c3798acbf973")]
 [assembly: System.Reflection.AssemblyProductAttribute("BU_BarcodeScanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BU_BarcodeScanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
